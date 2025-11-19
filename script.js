56d70d71f40f8536075f3ac4743a723209803ac6
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-19 18:35:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ for (let index = 0; index < totalSquare; index++) {
 
 	square.addEventListener('mouseover', ()=> {
 		const randomColor = getRandomColor();
+		square.style.backgroundColor = randomColor;
 
 		 setTimeout(() => {
-            square.style.backgroundColor = '#ddd';
+            square.style.backgroundColor = 'rgb(29, 29, 29)';
         }, 1000);
 	});
 	container.appendChild(square);
