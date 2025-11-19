748f144d78c0be8dfb7161cf48ebd25870f5a0ae
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-19 18:40:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ const container = document.querySelector('.container');
 const totalSquare = 800;
 
 for (let index = 0; index < totalSquare; index++) {
-	const square = document.createElement('div');
-	square.classList.add('square');
+    const square = document.createElement('div');
+    square.classList.add('square');
 
-	square.addEventListener('mouseover', ()=> {
-		const randomColor = getRandomColor();
-		square.style.backgroundColor = randomColor;
+    square.addEventListener('mouseover', () => {
+        const randomColor = getRandomColor();
 
-		 setTimeout(() => {
+        square.style.backgroundColor = randomColor;
+
+        setTimeout(() => {
             square.style.backgroundColor = 'rgb(29, 29, 29)';
         }, 1000);
-	});
-	container.appendChild(square);
+    });
+
+    container.appendChild(square);
 }
 
 function getRandomColor() {
-	return `hsl(${Math.random() * 360}, 100%, 50%)`;
+    return `hsl(${Math.random() * 360}, 100%, 50%)`;
 }
