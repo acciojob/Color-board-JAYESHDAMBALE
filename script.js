6d8ab7ec4ff91230632c298a0f4bd5ad8575dbda
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-19 18:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-const container = document.getElementById("container");
-const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
-const SQUARES = 800;
+const container = document.querySelector('.container');
+const totalSquare = 800;
 
-// Create 800 squares
-for (let i = 0; i < SQUARES; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+for (let index = 0; index < totalSquare; index++) {
+	const square = document.createElement('div');
+	square.classList.add('square');
 
-  // Hover effect
-  square.addEventListener("mouseover", () => setColor(square));
-  square.addEventListener("mouseleave", () => removeColor(square));
+	square.addEventListener('mouseover', ()=> {
+		const randomColor = getRandomColor();
 
-  container.appendChild(square);
+		 setTimeout(() => {
+            square.style.backgroundColor = '#ddd';
+        }, 1000);
+	});
+	container.appendChild(square);
 }
 
-// Set random color
-function setColor(element) {
-  const color = getRandomColor();
-  element.style.background = color;
-}
-
-// Remove color smoothly
-function removeColor(element) {
-  element.style.background = "#1d1d1d"; // fades out in 1s due to CSS transition
-}
-
-// Pick a random color
 function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+	return `hsl(${Math.random() * 360}, 100%, 50%)`;
 }
